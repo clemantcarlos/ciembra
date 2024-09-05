fc8ec4859cd3ebf141fffce39a76c073f3929ccd
clemant <clemantcarlos11@gmail.com>
2024-09-05 12:49:27 -0400

    change form to tsx and made it work

---
+++ src/components/common/PageForm.tsx
@@ -0,0 +1,222 @@
+import { submitHandler } from "@/lib/scripts.ts"
+
+export default function PageForm({children}:any){
+  return(
+    <form onSubmit={submitHandler} className="w-full flex flex-wrap justify-between gap-4 pt-4">
+            <div className="w-full md:w-fit">
+              <label htmlFor="nombre" 
+                className="block font-medium ">
+                Nombre completo*
+              </label>
+              <input className="bg-secondary text-primary text-sm rounded-lg focus:ring-none   w-full md:w-64 p-2.5" 
+                required 
+                type="text" 
+                name="nombre" 
+                id=""/>
+            </div>
+            <div className="w-full flex flex-col md:w-fit">
+              <label htmlFor="cargo" className="font-medium w-full">
+                Cargo*
+              </label>
+              <select className="bg-secondary text-primary text-sm rounded-lg focus:ring-none w-full md:w-64 p-2.5" name="Cargos" required>
+                  <option value="" disabled={false} selected={false} hidden={false}></option>
+                  <option value="Director(a) de Tecnologia – IT">
+                    Director de Tecnologia – IT
+                  </option>
+                  <option value="Coordinador(a) de Tecnologia – IT">
+                    Coordinador(a) de Tecnologia – TI
+                  </option>
+                  <option value="Analista de Tecnologia – TI">
+                    Analista de Tecnologia – TI
+                  </option>
+                  <option value="Director(a) Comercial">
+                    Director(a) Comercial
+                  </option>
+                  <option value="Coordinador(a) Comercial">
+                    Coordinador(a) Comercial
+                  </option>
+                  <option value="Analista Comercial">
+                    Analista Comercial
+                  </option>
+                  <option value="Director(a) de Marketing">
+                    Director(a) de Marketing
+                  </option>
+                  <option value="Coordinador(a) de Marketing">
+                    Coordinador(a) de Marketing
+                  </option>
+                  <option value="Analista de Marketing">
+                    Analista de Marketing
+                  </option>
+                  <option value="Director(a) de Operaciones">
+                    Director(a) de Operaciones
+                  </option>
+                  <option value="Coordinador(a) de Operaciones">
+                    Coordinador(a) de Operaciones
+                  </option>
+                  <option value="Técnico de Operaciones">
+                    Técnico de Operaciones
+                  </option>
+                  <option value="Director(a) de Compras">
+                    Director(a) de Compras
+                  </option>
+                  <option value="Coordinador(a) de Compras">
+                    Coordinador(a) de Compras
+                  </option>
+                  <option value="Analista de Compras">
+                    Analista de Compras
+                  </option>
+                  <option value="Director(a) de Innovacion">
+                    Director(a) de Innovacion
+                  </option>
+                  <option value="Coordinador(a) de Innovacion">
+                    Coordinador(a) de Innovacion
+                  </option>
+                  <option value="Analista de Negocios">
+                    Analista de Negocios
+                  </option>
+                  <option value="CEO/Fundador/Socio">
+                    CEO/Fundador/Socio
+                  </option>
+                  <option value="Estudiante/Becario(a)">
+                    Estudiante/Becario(a)
+                  </option>
+                  <option value="Otros Cargos">
+                    Otros Cargos
+                  </option> 
+                </select>
+            </div>
+            <div className="w-full md:w-fit">
+              <label htmlFor="nombreEmpresa" 
+                className="block font-medium ">
+                Nombre de la empresa*
+              </label>
+              <input className="bg-secondary text-primary text-sm rounded-lg focus:ring-non w-full md:w-64 p-2.5" 
+                type="text" 
+                name="nombreEmpresa" 
+                id=""/>
+            </div>
+            <div className="w-full md:w-fit">
+              <label htmlFor="areaMercado" 
+                className="block font-medium ">
+                Area de mercado*
+              </label>
+              <select className="bg-secondary text-primary text-sm rounded-lg focus:ring-none w-full md:w-64 p-2.5" name="mercado"  required>
+                <option value="" disabled={false} selected={false} hidden={false}></option>
+                <option value="Agropecuario">
+                  Agropecuario
+                </option>
+                <option value="Alimentos e Bebidas">
+                  Alimentos e Bebidas
+                </option>
+                <option value="Automotor">
+                  Automotor
+                </option>
+                <option value="Comercio mayorista y minorista">
+                  Comercio mayorista y minorista
+                </option>
+                <option value="Comercio Exterior">
+                  Comercio Exterior
+                </option>
+                <option value="Construccion civil e Imbobiliario">
+                  Construccion civil e Imbobiliario
+                </option>
+                <option value="Cooperativa de Produccion">
+                  Cooperativa de Produccion
+                </option>
+                <option value="Cuero y calzado">
+                  Cuero y calzado
+                </option>
+                <option value="Educacion">
+                  Educacion
+                </option>
+                <option value="Energia">
+                  Energia
+                </option>
+                <option value="Financiero">
+                  Financiero
+                </option>
+                <option value="Madera, papel y celulosa">
+                  Madera, papel y celulosa
+                </option>
+                <option value="Marketing y Comunicacion">
+                  Marketing y Comunicacion
+                </option>
+                <option value="Mecanica">
+                  Mecanica
+                </option>
+                <option value="Muebles">
+                  Muebles
+                </option>
+                <option value="Plastico y Caucho">
+                  Plastico y Caucho
+                </option>
+                <option value="Química e Petroquímica">
+                  Química e Petroquímica
+                </option>
+                <option value="Salud">
+                  Salud
+                </option>
+                <option value="Servicios">
+                  Servicios
+                </option>
+                <option value="Servicios Publicos">
+                  Servicios Publicos
+                </option>
+                <option value="Metalurgia y Mineria">
+                  Metalurgia y Mineria
+                </option>
+                <option value="Tecnologia de la Informacion">
+                  Tecnologia de la Informacion
+                </option>
+                <option value="Telecom">
+                  Telecom
+                </option>
+                <option value="Textil y confecciones">
+                  Textil y confecciones
+                </option>
+                <option value="Transporte y Logistica">
+                  Transporte y Logistica
+                </option>
+                <option value="Turismo">
+                  Turismo
+                </option>
+                <option value="Veterinario">
+                  Veterinario
+                </option>
+                <option value="Otro">
+                  Otro
+                </option> 
+                </select>
+            </div>
+            <div className="w-full md:w-fit">
+              <label htmlFor="email" 
+                className="block font-medium ">
+                E-mail*
+              </label>
+              <input className="bg-secondary text-primary text-sm rounded-lg focus:ring-non w-full md:w-64 p-2.5" 
+                type="text" 
+                name="email" 
+                id=""/>
+            </div>
+            <div className="w-full md:w-fit">
+              <label htmlFor="telefono" className="block font-medium ">
+                Telefono*
+              </label>
+              <input className="bg-secondary text-primary text-sm rounded-lg focus:ring-non w-full md:w-64 p-2.5"
+                type="text" 
+                name="telefono" 
+                id=""/>
+            </div>
+            <div className="w-full pl-10 pt-4">
+              <input type="checkbox" name="privacidad" id="privacidad"/>
+              <label htmlFor="privacidad">
+                Estoy deacuerdo con las 
+                <a href="politicasPrivacidad" className="font-bold text-emerald-300">Politicas de Privacidad</a>
+              </label>
+            </div>
+            <div className="w-full pl-10 pt-4">
+              {children}
+            </div>
+    </form>
+  )
+}
